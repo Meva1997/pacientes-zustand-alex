7e83f997f7f98ff419dce0d572860747d2243017
Alex Medina <alexmedina@192.168.1.88>
2025-05-30 12:35:00 -0600

    added base url

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/pacientes-veterinaria-zustand/',
+  base: '/pacientes-zustand-alex/',
   plugins: [
     react(),
     tailwindcss()
